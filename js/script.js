8cae16c4aef12126ed48b5ed1f59827e763436e2
Tiago Izaias <59627733+tiagoiziaas@users.noreply.github.com>
2024-04-29 18:57:27 -0300

    Meu Portifolio

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 const img = document.getElementById('img');
-const mut = document.querySelector('.mut')
-const bord =document.querySelector('.border-color')
+const mut = document.querySelector('.mut');
+const bord =document.querySelector('.border-color');
 const body = document.body;
 img.addEventListener('click', function clicar(){
  
   if(body.classList.toggle('black-color')){
       
       img.style.transition ="all .5s";
-      img.src = './imagens/icones/moon.png'
+      img.src = '/Portifolio/imagens/icones/moon.png';
       img.style.transform = 'translate(0%)';
       mut.style.border = '2px solid #ffff';
       mut.style.background = '#0c151d';
-      mut.style.boxShadow = '#ffff 3px 2px 2px'
+      mut.style.boxShadow = '#ffff 3px 2px 2px';
     }else{
-      img.style.transform = ' translate(142%)'
-      img.src = './imagens/icones/sun.png'; 
+      img.style.transform = ' translate(142%)';
+      img.src = '/Portifolio/imagens/icones/sun.png'; 
       mut.style.border = '2px solid #FF0B53';
       img.style.transition ="all .5s";
-      mut.style.boxShadow = '#FF0B53 3px 2px 2px'
+      mut.style.boxShadow = '#FF0B53 3px 2px 2px';
       mut.style.background = 'white';
     }
 });
 const btnp = document.querySelector('#proj');
-const btnk = document.querySelector('#skil')
+const btnk = document.querySelector('#skil');
 
 
 
